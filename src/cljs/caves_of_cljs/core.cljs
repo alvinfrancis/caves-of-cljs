f6932c625e56517e9cc9a29216d48acb7a277eaa
alvinfrancis <alvinfrancisdumalus@gmail.com>
2015-02-02 11:17:45 +0800

    Do not use session for state management

--- src/cljs/caves_of_cljs/core.cljs
+++ src/cljs/caves_of_cljs/core.cljs
@@ -1,34 +1,49 @@
 (ns caves-of-cljs.core
     (:require [reagent.core :as reagent :refer [atom]]
-              [reagent.session :as session]
               [secretary.core :as secretary :include-macros true]
               [goog.events :as events]
               [goog.history.EventType :as EventType])
     (:import goog.History))
 
 ;; -------------------------
+;; State
+
+(def app-state (atom {}))
+
+;; -------------------------
 ;; Views
 
 (defn home-page []
-  [:div [:h2 "Welcome to caves-of-cljs"]
-   [:div [:a {:href "#/about"} "go to about page"]]])
+  [:div [:h2 "Caves of Clojurescript"]
+   [:div [:a {:href "#/about"} "go to the about page"]]])
 
 (defn about-page []
   [:div [:h2 "About caves-of-cljs"]
    [:div [:a {:href "#/"} "go to the home page"]]])
 
-(defn current-page []
-  [:div [(session/get :current-page)]])
+(defmulti page #(:current-page (deref %)))
+
+(defmethod page nil [state]
+  [home-page])
+
+(defmethod page :home [state]
+  [home-page])
+
+(defmethod page :about [state]
+  [about-page])
+
+(defn current-page [app-state]
+  [:div [page app-state]])
 
 ;; -------------------------
 ;; Routes
 (secretary/set-config! :prefix "#")
 
 (secretary/defroute "/" []
-  (session/put! :current-page #'home-page))
+  (swap! app-state assoc :current-page :home))
 
 (secretary/defroute "/about" []
-  (session/put! :current-page #'about-page))
+  (swap! app-state assoc :current-page :about))
 
 ;; -------------------------
 ;; History
@@ -45,4 +60,4 @@
 ;; Initialize app
 (defn init! []
   (hook-browser-navigation!)
-  (reagent/render-component [current-page] (.getElementById js/document "app")))
+  (reagent/render-component [current-page app-state] (.getElementById js/document "app")))
