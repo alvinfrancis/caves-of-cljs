b2ee2418e1541473e586b73b8cae8f25e8ca5d1b
alvinfrancis <alvinfrancisdumalus@gmail.com>
2015-02-03 22:55:51 +0800

    Add utils namespace

---
+++ src/cljs/caves_of_cljs/utils.cljs
@@ -0,0 +1,33 @@
+(ns caves-of-cljs.utils
+  (:require [goog.dom :as dom]
+            [goog.events :as events]
+            [cljs.core.async :as async :refer [put! chan <!]]))
+
+(defn dissoc-in [data path]
+  (let [prefix (butlast path)
+        key (last path)]
+    (if prefix
+      (update-in data prefix dissoc key)
+      (dissoc data key))))
+
+(defn log-clj [o]
+  (.log js/console (clj->js o)))
+
+(defn domain [url]
+  (let [re (new js/RegExp "^https?\\:\\/\\/(www\\.)?([^\\/:?#]+)(?:[\\/:?#]|$)")
+        matches (.exec re url)]
+    (and matches (get matches 2))))
+
+(defn keys-in [m]
+  (if (map? m)
+    (mapcat (fn [[k v]]
+              (let [sub (keys-in v)]
+                (conj sub k)))
+            m)
+    []))
+
+(defn listen [el type]
+  (let [out (chan)]
+    (events/listen el type
+      (fn [e] (put! out e)))
+    out))
